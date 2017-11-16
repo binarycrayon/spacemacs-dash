ecfa923b5197d3dc4933a2f2022d283761bd2b1d
Yudi Xue <yudi.xue@usask.ca>
2017-11-15 21:29:52 -0800

    more tailored documentation

--- spacemacsdoc2set.py
+++ spacemacsdoc2set.py
@@ -9,9 +9,9 @@ DOC_DIR = 'doc'
 DOCS = [
     ('quick_start.html', 'Guide'),
     ('beginners_tutorial.html', 'Guide'),
+    ('layers.html', 'Guide'),
     ('vimusers.html', 'Guide'),
-    ('documentation.html', 'Framework'),
-    ('layers.html', 'Library')
+    ('documentation.html', 'Library'),
 ]
 
 
@@ -27,20 +27,40 @@ load_page = lambda doc: BeautifulSoup(open(get_full_doc_path(doc)).read(), 'html
 
 def build_doc(cursor, doc_type, doc):
     org_tag = re.compile('#.')
-    num_text = re.compile('^\d+\.\d+.')
+    num_text = re.compile('^(\d+\.).')
     text_regex = re.compile('\.\s([A-z\s\-]+)')
     soup = load_page(doc)
+    cmd_chapter = None
+    setting_chapter = None
+    ignore_chapter = None
 
     if doc_type == 'Guide':
         name = soup.find('title').text.strip()
         insert_to_db(cursor, name, doc_type, get_doc_path(doc))
 
-    elif doc_type in ('Framework', 'Library'):
+    else:
+
         for tag in soup.find_all('a', {'href':org_tag}, text=num_text):
-            name = text_regex.findall(tag.text.strip())[-1]
+            text = tag.text.strip()
+            name = text_regex.findall(text)[-1]
+            chapter = num_text.findall(text)[-1]
+            if 'Commands' in name:
+                cmd_chapter = chapter
+            elif 'Dotfile Configuration' in name:
+                setting_chapter = chapter
+            elif 'Achievements' in name or 'Thank you' in name:
+                ignore_chapter = chapter
+
             path = get_doc_path(doc) + tag.attrs['href'].strip()
-            insert_to_db(cursor, name, doc_type, path)
-            print ('name: {}, type: {} path: {}'.format(name, doc_type, path))
+
+            if cmd_chapter and text.startswith(cmd_chapter):
+                insert_to_db(cursor, name, 'Command', path)
+            elif setting_chapter and text.startswith(setting_chapter):
+                insert_to_db(cursor, name, 'Setting', path)
+            elif ignore_chapter and text.startswith(ignore_chapter):
+                continue
+            else:
+                insert_to_db(cursor, name, 'Library', path)
 
 
 if __name__ == '__main__':
